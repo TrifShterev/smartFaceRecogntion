dd5c25a24dd07520060cd44682076a71c09f9e5e
trifon shterev <tshterev@abv.bg>
2021-01-30 21:25:04 +0200

    front end components

--- smartfacerecognitionbrain/src/App.js
+++ smartfacerecognitionbrain/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js'
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+import 'tachyons'
+
+
+
+const particlesOptions = {
+  
+    particles: {
+      particles:{
+        number:{
+          value: 30,
+          density:{
+            enable: true,
+            value_area: 800
+          }
+        }
+      }
+    }
+  }
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Particles className= 'particles'
+         params={particlesOptions}
+               / >
+      <Navigation />
+       <Logo />
+       <Rank />
+    <ImageLinkForm />
+      {/*  <FaceRecognition /> */}
     </div>
   );
 }
---
+++ smartfacerecognitionbrain/src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () =>{
+    return (
+    <div >
+    <div className= 'white f3'>
+        {'Trifon your current rank is...'}
+    </div>
+    <div className= 'white f1'>
+        {'#3'}
+    </div>
+    </div>
+     );
+}
+
+export default Rank;
