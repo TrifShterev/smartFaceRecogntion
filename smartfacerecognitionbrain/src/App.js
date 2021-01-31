10622bdda40c3ec3bfcd51a6003f4c86acd1acd6
trifon shterev <tshterev@abv.bg>
2021-01-31 02:13:01 +0200

    frontEnd Componens done

--- smartfacerecognitionbrain/src/App.js
+++ smartfacerecognitionbrain/src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js'
+import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
@@ -8,6 +10,7 @@ import './App.css';
 import 'tachyons'
 
 
+const app = new Clarifai.App({apiKey: 'f705a40cd5444e039555c7a03ac3483b'});
 
 const particlesOptions = {
   
@@ -25,19 +28,48 @@ const particlesOptions = {
   }
 
 
-function App() {
-  return (
-    <div className="App">
-       <Particles className= 'particles'
-         params={particlesOptions}
-               / >
-      <Navigation />
-       <Logo />
-       <Rank />
-    <ImageLinkForm />
-      {/*  <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange= (event) =>{
+this.setState({input: event.target.value});
+  }
+onButtonSubmit=()=>{
+  this.setState({imageUrl:this.state.input});
+
+  app.models.predict(Clarifai.FACE_DETECT_MODEL,
+   this.state.input)
+  .then(
+function(response){
+console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+},
+function(err){
+
+}
+  )
+    
+}
+  render(){
+    return (
+      <div className="App">
+         <Particles className= 'particles'
+           params={particlesOptions}
+                 / >
+        <Navigation />
+         <Logo />
+         <Rank />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+         <FaceRecognition imageUrl={this.state.imageUrl}/> 
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
