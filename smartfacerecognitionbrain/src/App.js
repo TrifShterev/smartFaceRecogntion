2c48cc0f2eaedb93b970777a404eb7cc548cdb0b
trifon shterev <tshterev@abv.bg>
2021-02-06 23:41:07 +0200

    faceBox start

--- smartfacerecognitionbrain/src/App.js
+++ smartfacerecognitionbrain/src/App.js
@@ -33,9 +33,31 @@ class App extends Component {
     super();
     this.state={
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box:{},
     }
   }
+
+calculateFaceLocation=(data)=>{
+
+const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+
+const image = document.getElementById('inputimage');
+const width= Number(image.width);
+const height= Number(image.height);
+return {
+  leftCol: clarifaiFace.left_col * width,
+  topRow: clarifaiFace.top_row*height,
+  rightCol: width - (clarifaiFace.right_col*width),
+  bottomRow: height - (clarifaiFace.bottom_row*height)
+}
+
+}
+
+displayFaceBox= (box)=>{
+
+  this.setState({box: box});
+}
 
   onInputChange= (event) =>{
 this.setState({input: event.target.value});
@@ -45,14 +67,11 @@ onButtonSubmit=()=>{
 
   app.models.predict(Clarifai.FACE_DETECT_MODEL,
    this.state.input)
-  .then(
-function(response){
-console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-},
-function(err){
+  .then(response =>this.displayFaceBox(this.calculateFaceLocation(response)))
+  .catch(err=> console.log(err));  
 
-}
-  )
+
+  
     
 }
   render(){
