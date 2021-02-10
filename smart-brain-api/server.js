f6b6e837aec9d5cd0e970efe2d846dc0089d5787
trifon shterev <tshterev@abv.bg>
2021-02-10 20:17:31 +0200

    SignIn and Register

---
+++ smart-brain-api/server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const database={
+    users:[{
+        id: '123',
+        name:'John',
+        email:'john@gmail.com',
+        password:'cookies',
+        entries: 0,
+        joined: new Date()
+    },
+    {
+        id: '124',
+        name:'Sally',
+        email:'sally@gmail.com',
+        password:'bananas',
+        entries: 0,
+        joined: new Date()
+    }]
+}
+
+app.get('/', (req,res)=>{
+    res.sendStatus(database.users)
+})
+
+app.post('/signin', (req,res)=>{
+    if (req.body.email== database.users[0].email &&
+        req.body.password==database.users[0].password) {
+        
+            res.json('success')
+    }else{
+        req.statusCode(400).json('error logging in')
+    }
+    
+})
+
+app.post('/register', (req,res) => {
+
+    const {email,name,password}=req.body;
+        database.users.push( {
+           id: '125',
+           name:'name',
+           email:'email',
+           password:'password',
+           entries: 0,
+           joined: new Date()
+        })
+
+        res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id', (req,res)=>{
+const {id} =req.params;
+let found= false;
+
+database.users.forEach(user=>{
+    if (user.id==id) {
+        
+        found=true;
+      return  res.json(user);
+    } 
+    
+})
+
+if(!found){
+
+   res.status(404).json('not found');
+    
+}
+
+})
+
+app.put('/image', (req,res)=> {
+    const {id} =req.body;
+    let found= false;
+    
+    database.users.forEach(user=>{
+        if (user.id===id) {
+            
+            found=true;
+            user.entries++;
+          return  res.json(user.entries);
+        } 
+        if(!found){
+
+            res.status(404).json('not found');
+             
+         }
+    })
+
+})
+
+app.listen(3000, ()=>{
+    console.log('app is running on port 3000');
+})
+
+
+/*  --> TODO--> respond = this is working
+ /SignIN = post--> Success/fail
+/register= POST -->user
+ /profile/:userId --> GET = user
+ /image --> PUT --> user
+*/
